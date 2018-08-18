15b14f9a2715e8df3bb2a25f972c35307ea8d997
Luciano Paci <lucianopaci93@gmail.com>
2018-08-18 18:23:14 -0300

    Added Profile Routing

--- validation/login.js
+++ validation/login.js
@@ -3,11 +3,12 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateLoginInput(data) {
   let errors = {};
+  
+  // This validation is used to set possible 'null' or 'undefined' to Strings, in order
+  // to use Validator correctly
 
-  data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   
 

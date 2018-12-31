50cc28a78fbb2bedac05731fbc02e751e1126894
Luciano Paci <lucianopaci93@gmail.com>
2018-12-31 17:49:24 -0300

    Modified keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-	mongoURI: 'mongodb://paci:123qwe@ds123562.mlab.com:23562/socialnetwork',
+	mongoURI:
+		'mongodb://luciano:Libertadores2018@lpcluster-shard-00-00-xhh9r.mongodb.net:27017,lpcluster-shard-00-01-xhh9r.mongodb.net:27017,lpcluster-shard-00-02-xhh9r.mongodb.net:27017/test?ssl=true&replicaSet=LPCluster-shard-0&authSource=admin&retryWrites=true',
 	secretOrKey: 'secret'
 }
